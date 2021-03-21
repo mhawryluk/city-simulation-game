a8348588cf2c08e76e1718a09ae930b3fe0101d3
mhawryluk <marcinhawrylukxx@gmail.com>
2021-03-22 00:08:35 +0100

    added movement across the map

--- src/GameModes/GameWindow.py
+++ src/GameModes/GameWindow.py
@@ -6,12 +6,35 @@ class GameWindow(GameMode):
     def __init__(self, window, save, height, width):
         super().__init__(window, save)
         self.city_space = CitySpace(height, width)
+        self.SCROLL_SPEED = 5
 
     def update(self):
+        self.city_space.update()
         self.draw()
 
     def handle(self, event):
-        pass
+        if event.type == pg.KEYDOWN:
+            # moving across the map
+            if event.key == pg.K_d:
+                self.city_space.add_move_speed((self.SCROLL_SPEED, 0))
+            elif event.key == pg.K_s:
+                self.city_space.add_move_speed((0, self.SCROLL_SPEED))
+            elif event.key == pg.K_a:
+                self.city_space.add_move_speed((-self.SCROLL_SPEED, 0))
+            elif event.key == pg.K_w:
+                self.city_space.add_move_speed((0, -self.SCROLL_SPEED))
+
+        if event.type == pg.KEYUP:
+            # moving across the map
+            if event.key == pg.K_d:
+                self.city_space.add_move_speed((-self.SCROLL_SPEED, 0))
+            elif event.key == pg.K_s:
+                self.city_space.add_move_speed((0, -self.SCROLL_SPEED))
+            elif event.key == pg.K_a:
+                self.city_space.add_move_speed((self.SCROLL_SPEED, 0))
+            elif event.key == pg.K_w:
+                self.city_space.add_move_speed((0, self.SCROLL_SPEED))
 
     def draw(self):
+        self.window.fill((0, 0, 0))
         self.city_space.draw(self.window)
