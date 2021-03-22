1099f1be86d4e1adf7ee13b981f756df57694c3f
Singularity23x0 <dawidpastuszka23x0@gmail.com>
2021-03-22 20:58:08 +0100

    MM: ob-click event; SM: part of save handling

--- src/GameEngine.py
+++ src/GameEngine.py
@@ -46,7 +46,21 @@ class SaveManager:
     def __init__(self):
         self.active_save = None
         self.active_settings = None
+        self.saves_list = []
+        self.load_settings()
 
+    def generate_base_settings(self, settings_file):
+        settings = dict()
+        settings['active_save'] = None
+        settings['saves_amount'] = 0
+        settings['max_saves_amount'] = 500
+        with open(settings_file, 'w') as new_settings_file:
+            js.dump(settings, new_settings_file)
+
+    def generate_game_save_source(self):
+        pass
+
+    def load_settings(self):
         settings_file = os.path.join('SaveFiles', 'settings.json')
 
         if not os.path.isfile(settings_file):
@@ -55,26 +69,31 @@ class SaveManager:
         with open(settings_file, 'r') as settings:
             self.active_settings = js.load(settings)
 
-        self.load_save(self.active_settings.get('active_save', None))
-
-    def generate_base_settings(self, settings_file):
-        settings = dict()
-        settings['active_save'] = None
-        with open(settings_file, 'w') as new_settings_file:
-            js.dump(settings, new_settings_file)
-
-    def generate_game_save_source(self):
-        pass
-
-    def load_settings(self):
-        pass
-
     def save_settings(self):
         pass
 
+    def create_save(self, name):
+        save_id = settings['saves_amount'] + 1
+        if save_id >= settings['max_saves_amount']:
+            #error
+        else:
+            settings['saves_amount'] = save_id
+            self.active_save = {'name':name, 'id':save_id}
+
+            self.save()
+
+    def activate_save(self, save_id):
+        self.active_settings['active_save'] = save_id
+
     def load_save(self, save_id):
-        if save_id != None:
+        try:
+            pass
+        except:
             pass
 
     def save(self):
-        pass
+        save_id = self.activate_save['id']
+        self.active_save['last_saved'] = pg.time.get_time()
+        save_path = os.path.join('SaveFiles', 'save' + save_id + '.json')
+        with open(save_path, 'w') as save_file:
+            js.dump(self.activate_save, save_file)
--- src/GameModes/MainMenu.py
+++ src/GameModes/MainMenu.py
@@ -9,26 +9,44 @@ class MainMenu(GameMode):
         self.change_mode = False
         self.redraw = True
         self.WINDOW = WINDOW
+
+        self.background = None
+        self.menu = None
+        self.play_button = None
+
+        self.create_menu_window()
+
+    def create_menu_window(self):
+    
         width, height = self.WINDOW.get_size()
 
         self.background = pg.image.load(os.path.join('Assets', 'menu_background.jpg'))
         self.background = pg.transform.scale(self.background, self.WINDOW.get_size())
 
-        self.menu = pgmen.Menu(title='City Simulation Game', width=width*0.6, height=height*0.85, theme=pgmen.themes.THEME_SOLARIZED)
+        self.menu = pgmen.Menu(title='City Simulation Game', width=width*0.6, height=height*0.85, theme=pgmen.themes.THEME_SOLARIZED, 
+        mouse_enabled=True, mouse_motion_selection=True)
+
+        self.play_button = self.menu.add.button('Play', self.play)
 
     def redraw_needed(self):
         self.redraw = not self.redraw
         return not self.redraw
 
     def update(self):
+        self.draw()
+        
+
+    def play(self):
+        #configure save
+        #self.change_mode = True  
+        print("PLAY")          
+
+    def handle(self, event):
+        if self.menu.is_enabled():
+            self.menu.update([event])
+            self.draw()
+
+    def draw(self):
         self.WINDOW.blit(self.background, (0,0))
         self.menu.draw(self.WINDOW)
         pg.display.update()
-
-            
-
-    def handle(self, event):
-        next_game_mode = self
-
-    def draw(self):
-        pass
