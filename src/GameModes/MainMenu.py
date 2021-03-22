c37555b1f261d7cd07bbcde065be51e251cb871f
Singularity23x0 <dawidpastuszka23x0@gmail.com>
2021-03-22 16:28:06 +0100

    Main menu: added background and menu box

--- src/GameModes/MainMenu.py
+++ src/GameModes/MainMenu.py
@@ -1,5 +1,6 @@
 from GameModes.GameMode import *
 import pygame as pg
+import pygame_menu as pgmen
 import os
 
 class MainMenu(GameMode):
@@ -8,18 +9,23 @@ class MainMenu(GameMode):
         self.change_mode = False
         self.redraw = True
         self.WINDOW = WINDOW
+        width, height = self.WINDOW.get_size()
 
         self.background = pg.image.load(os.path.join('Assets', 'menu_background.jpg'))
         self.background = pg.transform.scale(self.background, self.WINDOW.get_size())
+
+        self.menu = pgmen.Menu(title='City Simulation Game', width=width*0.6, height=height*0.85, theme=pgmen.themes.THEME_SOLARIZED)
 
     def redraw_needed(self):
         self.redraw = not self.redraw
         return not self.redraw
 
     def update(self):
-        if self.redraw_needed():
-            self.WINDOW.blit(self.background, (0,0))
-            pg.display.update()
+        self.WINDOW.blit(self.background, (0,0))
+        self.menu.draw(self.WINDOW)
+        pg.display.update()
+
+            
 
     def handle(self, event):
         next_game_mode = self
