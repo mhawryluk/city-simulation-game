8e3e037df92a785ca17ffbb670e43d75411736dd
mhawryluk <marcinhawrylukxx@gmail.com>
2021-03-25 23:59:03 +0100

    more images drawn

--- src/City/CityImages.py
+++ src/City/CityImages.py
@@ -1,15 +1,26 @@
 from City.LotType import *
 import pygame as pg
 import os
+from random import random, choice
 
 
 class CityImages:
     def __init__(self):
         self.images = {
-            LotType.GRASS: pg.image.load(
-                os.path.join('Assets', 'grass.png')),
-            LotType.WATER: pg.image.load(
-                os.path.join('Assets', 'water.png'))}
+            LotType.GRASS: {
+                "main": pg.image.load(os.path.join('Assets', 'grass.png')),
+                "additional": [
+                    pg.image.load(os.path.join('Assets', 'hills.png')),
+                    pg.image.load(os.path.join('Assets', 'flowers.png'))
+                ],
+            },
+            LotType.WATER: {
+                "main": pg.image.load(os.path.join('Assets', 'water.png')),
+                "additional": [pg.image.load(os.path.join('Assets', 'island.png'))]
+            }}
 
-    def get_image(self, lot_type):
-        return self.images[lot_type]
+    def get_images(self, lot_type):
+        images = [self.images[lot_type]["main"]]
+        if self.images[lot_type]["additional"] and random() < 0.1:
+            images += [choice(self.images[lot_type]["additional"])]
+        return images
