50cae389dd39afbe84e971946c6fc7cbd691e898
Singularity23x0 <dawidpastuszka23x0@gmail.com>
2021-03-26 18:27:40 +0100

    Changed naming convention in save manager

---
+++ SaveFiles/readme.txt
@@ -0,0 +1,4 @@
+Data save folder.
+Not to be tampered with if you do not know what you're doing!
+Data saved as python dictionaries in json files.
+The 'save_manager_data.json' file contains all basic settings and data used by the SaveManager to work properly.
--- src/GameEngineTools/SaveManager.py
+++ src/GameEngineTools/SaveManager.py
@@ -6,50 +6,50 @@ from time import gmtime, strftime
 class SaveManager:
     def __init__(self):
         self.active_save = None
-        self.settings = None
+        self.sm_data = None
         self.saves_list = []
-        self.load_settings()
+        self.load_save_manager_data()
 
-    def generate_base_settings(self, settings_file):
-        settings = dict()
-        settings['active_save'] = None
-        settings['max_saves_amount'] = 500
-        settings['max_save_id'] = 0
-        settings['free_save_id_list'] = []
-        with open(settings_file, 'w') as new_settings_file:
-            js.dump(settings, new_settings_file)
+    def generate_save_manager_data_to(self, sm_data_file_path):
+        sm_data = dict()
+        sm_data['active_save'] = None
+        sm_data['max_saves_amount'] = 500
+        sm_data['max_save_id'] = 0
+        sm_data['free_save_id_list'] = []
+        with open(sm_data_file_path, 'w') as new_sm_data_file:
+            js.dump(sm_data, new_sm_data_file)
 
     def get_game_save(self):
         pass
 
-    def load_settings(self):
-        settings_file = os.path.join('SaveFiles', 'settings.json')
+    def load_save_manager_data(self):
+        sm_data_file_path = os.path.join('SaveFiles', 'save_manager_data.json')
 
-        if not os.path.isfile(settings_file):
-            self.generate_base_settings(settings_file)
+        if not os.path.isfile(sm_data_file_path):
+            self.generate_save_manager_data_to(sm_data_file_path)
 
-        with open(settings_file, 'r') as settings:
-            self.settings = js.load(settings)
+        with open(sm_data_file_path, 'r') as sm_data:
+            self.sm_data = js.load(sm_data)
 
-    def save_settings(self):
-        settings_file = os.path.join('SaveFiles', 'settings.json')
+    def save_save_manager_data(self):
+        sm_data_file_path = os.path.join('SaveFiles', 'save_manager_data.json')
 
-        with open(settings_file, 'w') as settings:
-            js.dump(self.settings, settings)
+        with open(sm_data_file_path, 'w') as sm_data:
+            js.dump(self.sm_data, sm_data)
 
     def has_active_save(self):
-        return self.settings['active_save'] != None
+        return self.sm_data['active_save'] != None
 
     def create_save(self, name):
-        free_save_id = self.settings['free_save_id_list']
-        save_id = self.settings['max_save_id'] + 1
+        free_save_id = self.sm_data['free_save_id_list']
+        save_id = self.sm_data['max_save_id'] + 1
         if len(free_save_id) > 0:
             save_id = free_save_id.pop()
 
-        if self.settings['max_save_id'] + 1 >= self.settings['max_saves_amount'] and len(free_save_id) == 0:
+        if self.sm_data['max_save_id'] + 1 >= self.sm_data['max_saves_amount'] and len(free_save_id) == 0:
             return 'You have reached your save limit'
         else: 
-            self.settings['max_save_id'] = max(save_id, self.settings['max_save_id'])
+            self.sm_data['max_save_id'] = max(save_id, self.sm_data['max_save_id'])
             self.active_save = (save_id, name, {
                 'created' : strftime("%Y-%m-%d %H:%M:%S", gmtime())
             }) #generate base save presets in dict
@@ -57,8 +57,8 @@ class SaveManager:
             return 'Save ' + name + ' created successfully'
 
     def delete_save(self, save_id):
-        max_save_id = self.settings['max_save_id']
-        free_save_id = [int(id) for id in self.settings['free_save_id_list']]
+        max_save_id = self.sm_data['max_save_id']
+        free_save_id = [int(id) for id in self.sm_data['free_save_id_list']]
         if save_id > max_save_id or save_id in free_save_id:
             return 'Error! No such save exists!'
         else:
@@ -67,16 +67,16 @@ class SaveManager:
                 max_save_id -= 1
             print(free_save_id)
             free_save_id = [id for id in free_save_id if id < max_save_id]
-            self.settings['max_save_id'] = max_save_id
-            self.settings['free_save_id_list'] = free_save_id
-            del self.settings[str(save_id)]
+            self.sm_data['max_save_id'] = max_save_id
+            self.sm_data['free_save_id_list'] = free_save_id
+            del self.sm_data[str(save_id)]
             path = os.path.join('SaveFiles', 'save' + str(save_id) + '.json')
             os.remove(path)
-            self.save_settings()
+            self.save_save_manager_data()
             return 'Save deleted successfully'
 
     def activate_save(self, save_id):
-        self.settings['active_save'] = save_id
+        self.sm_data['active_save'] = save_id
 
     def load_save(self, save_id):
         try:
@@ -87,8 +87,8 @@ class SaveManager:
     def save(self):
         save_id, save_name, save_data = self.active_save
         save_data['last_saved'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        self.settings[str(save_id)] = save_name
-        self.save_settings()
+        self.sm_data[str(save_id)] = save_name
+        self.save_save_manager_data()
         save_path = os.path.join('SaveFiles', 'save' + str(save_id) + '.json')
         with open(save_path, 'w') as save_file:
             js.dump(save_data, save_file)
