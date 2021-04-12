3b6f9ada7ab36b8e6d8ad2b19dd96f8bc5b87450
mhawryluk <marcinhawrylukxx@gmail.com>
2021-04-12 15:51:17 +0200

    more tiny color adjustments

--- src/GameModes/Panel.py
+++ src/GameModes/Panel.py
@@ -11,17 +11,16 @@ class Panel:
     def get_theme(self):
         greyness = 60
         theme = pgmen.themes.THEME_DARK.copy()
-        theme.widget_font = pg.font.SysFont("segoeuisymbol", 80)
         theme.title_font = pgmen.font.FONT_FRANCHISE
         theme.title_font_size = 40
+        theme.title_font_color = (230, 230, 230, 80)
         theme.title_background_color = (greyness, greyness, greyness)
         theme.background_color = (greyness, greyness, greyness, 70)
         theme.title_bar_style = pgmen.widgets.MENUBAR_STYLE_NONE
         theme.widget_font = pgmen.font.FONT_FRANCHISE
         theme.cursor_color = (200, 75, 100)
-        # theme.widget_border_color = (220, 50, 60)
-        # theme.widget_border_width = 5
         theme.widget_font_size = 30
+        theme.widget_font_color = theme.title_font_color
         # theme.widget_padding = 5
         return theme
 
