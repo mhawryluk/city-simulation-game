fbc7f4d81ad1a1adce64273f5cf53a1dfa0caf2d
mhawryluk <marcinhawrylukxx@gmail.com>
2021-05-13 23:31:19 +0200

    added changing working directory
    ...so the game can be run from anywhere

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 from game_engine import GameEngine
+from os import chdir, path, getcwd
+
+
+def change_directory():
+    if __file__ == 'main.py':
+        chdir('..')
+    else:
+        chdir(path.dirname(path.dirname(__file__)))
 
 
 def main():
+    change_directory()
     game_engine = GameEngine()
     game_engine.run()
 
