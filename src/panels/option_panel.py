746f37e646b5cc2ddf8ecd0374425f6d0b752839
mhawryluk <marcinhawrylukxx@gmail.com>
2021-05-16 00:54:05 +0200

    added volume control in options

--- src/panels/option_panel.py
+++ src/panels/option_panel.py
@@ -1,6 +1,7 @@
 import pygame_menu as pgmen
 from panels.panel import Panel
 from test import test
+import pygame as pg
 
 
 class OptionPanel(Panel):
@@ -12,15 +13,26 @@ class OptionPanel(Panel):
                                theme=self.get_theme(),
                                mouse_enabled=True, mouse_motion_selection=True,
                                enabled=False)
-        self.save_button = self.menu.add.button(
-            "save", self.save)
-        self.main_menu_button = self.menu.add.button(
-            "main menu", self.main_menu)
-        self.test_button = self.menu.add.button(
-            "test", test(game_window))
+        self.menu.add.button("save", self.save)
+        self.menu.add.button("main menu", self.main_menu)
+
+        self.menu.add.label('')
+        self.menu.add.button('volume up', self.volume_up)
+        self.menu.add.button('volume down', self.volume_down)
+        self.menu.add.label('')
+
+        self.menu.add.button("test", test(game_window))
 
     def main_menu(self):
         self.game_window.change_mode = True
+    
+    def volume_up(self):
+        volume = pg.mixer.music.get_volume()
+        pg.mixer.music.set_volume(min(1, volume + 0.1))
+    
+    def volume_down(self):
+        volume = pg.mixer.music.get_volume()
+        pg.mixer.music.set_volume(max(0, volume - 0.1))
 
     def save(self):
         self.game_window.save()
