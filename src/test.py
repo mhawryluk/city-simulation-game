e949aacb1fde64d208f4c6c9189baf139fa7cb8c
Singularity23x0 <dawidpastuszka23x0@gmail.com>
2021-05-29 12:08:00 +0200

    Fixet orientation bug

--- src/test.py
+++ src/test.py
@@ -3,6 +3,6 @@ def test(game_window):
         print('TEST')
         for row in game_window.city_space.lots:
             for lot in row:
-                print(lot.show(), end=' ')
+                print(lot.show(), end=' | ')
             print('---')
     return tst
